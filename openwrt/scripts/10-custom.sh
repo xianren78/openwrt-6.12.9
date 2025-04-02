c6744ff005a171171584321e63b001f678babbe3
闲人一枚 <qgtxqj@gmail.com>
2025-04-02 13:14:37 +0800

    remove mbedtls gcc15 patch

--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -2,6 +2,9 @@
 
 # 自定义脚本
 
+# remove mbedtls gcc15 patch after openwrt commit https://github.com/openwrt/openwrt/commit/53ab5629c31f3c7ca58cb48518d530c660ee0023
+rm -f package/libs/mbedtls/patches/901-tests-fix-string-initialization-error-on-gcc15.patch
+
 # add openwrt-nikki
 rm -rf package/new/helloworld/luci-app-nikki
 rm -rf package/new/helloworld/nikki
